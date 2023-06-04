582d459f7d672dfc36799f9e9f08f543815f64ba
surbaniec <radsler1999@gmail.com>
2023-06-04 17:19:06 +0200

    Fix ChartWrapper

--- components/ChartWrapper.tsx
+++ components/ChartWrapper.tsx
@@ -4,7 +4,7 @@ import Chart from './Chart';
 
 const ChartWrapper = () => {
   return (
-    <div className='md:w-[362] lg:w-full lg:col-span-2 bg-white flex justify-center rounded-md border py-2 px-10'>
+    <div className='md:w-[362] lg:w-full lg:col-span-2 bg-white flex justify-center rounded-md border py-2  md:px-2 lg:px-10'>
       <Chart />
     </div>
   );
